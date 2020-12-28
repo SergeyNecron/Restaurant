76944c9ebe1ffd3c905e413bded7991a0b668715
Sergey Muratkin <sergeymuratkin@yandex.ru>
2020-12-28 14:05:35 +0300

    conversion to war for TomCat, add WarRun.bat

---
+++ WarRun.bat
@@ -0,0 +1,2 @@
+cd target
+java -jar saloon-voting-1.0.war
--- src/main/java/ru/study/springboot/SaloonVotingApplication.java
+++ src/main/java/ru/study/springboot/SaloonVotingApplication.java
@@ -2,9 +2,10 @@ package ru.study.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SaloonVotingApplication {
+public class SaloonVotingApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SaloonVotingApplication.class, args);
