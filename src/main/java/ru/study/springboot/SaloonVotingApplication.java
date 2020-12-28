9d2de386dca997a082931065f674033f8cfb4f46
Sergey Muratkin <sergeymuratkin@yandex.ru>
2020-12-28 11:49:42 +0300

    Spring Boot, Maven, Lombok, Spring Web, Spring Data JPA, H2 Database

---
+++ src/main/java/ru/study/springboot/SaloonVotingApplication.java
@@ -0,0 +1,13 @@
+package ru.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaloonVotingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaloonVotingApplication.class, args);
+    }
+
+}
